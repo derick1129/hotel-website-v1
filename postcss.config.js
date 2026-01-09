b439e87c50114dbb820dde34b29353d724383a04
derick1129 <derickjohn1129@gmail.com>
2026-01-09 19:50:21 +0530

    Fix CSS appearance property compatibility - add standard property alongside webkit-appearance

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,30 @@
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 
+function appearancePlugin() {
+  return {
+    postcssPlugin: 'appearance-plugin',
+    Declaration: {
+      '-webkit-appearance': (decl) => {
+        // Add standard appearance property alongside webkit-appearance
+        const standardAppearance = decl.clone()
+        standardAppearance.prop = 'appearance'
+        decl.after(standardAppearance)
+      }
+    }
+  }
+}
+appearancePlugin.postcss = true
+
 export default {
   plugins: [
     tailwindcss,
-    autoprefixer,
+    autoprefixer({
+      grid: true,
+      flexbox: 'no-2009',
+      cascade: true,
+      add: true,
+    }),
+    appearancePlugin,
   ],
 };
